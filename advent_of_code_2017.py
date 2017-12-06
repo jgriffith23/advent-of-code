352e276e3bbad0ef1b1aec189b009340aadd59d8
Jennifer <jennifer.gd42@gmail.com>
2017-12-06 13:52:58 -0800

    2017 day 2 complete

--- advent_of_code_2017.py
+++ advent_of_code_2017.py
@@ -54,3 +54,68 @@ def check_sum_2(n_str):
             accumulator = accumulator + int(curr)
 
     return accumulator
+
+
+# Day 2, part 1
+
+def check_spreadsheet_sum(sheet):
+    """Return the checksum for a spreadsheet.
+
+    Instructions:
+    "For each row, determine the difference between the largest value and the 
+     smallest value; the checksum is the sum of all of these differences."
+
+    >>> sheet = "data/day2_test_data.txt"
+    >>> check_spreadsheet_sum(sheet)
+    18
+    """
+
+    checksum = 0
+
+    with open(sheet) as spreadsheet:
+        for row in spreadsheet:
+            row = row.rstrip().split()
+            row = [int(num) for num in row]
+
+            largest = max(row)
+            smallest = min(row)
+
+            checksum = checksum + (largest - smallest)
+
+    return checksum
+
+
+# Day 2, part 2
+
+def check_spreadsheet_sum2(sheet):
+    """Return the checksum for a spreadsheet.
+
+    Instructions:
+    "Find the only two numbers in each row where one evenly divides the other.
+    That is, where the result of the division operation is a whole number. Find
+    those numbers on each line, divide them, and add up each line's result."
+
+    >>> sheet = "data/day2_part2_test_data.txt"
+    >>> check_spreadsheet_sum2(sheet)
+    9
+    """
+
+    checksum = 0
+
+    with open(sheet) as spreadsheet:
+        for row in spreadsheet:
+            row = row.rstrip().split()
+            row = [int(num) for num in row]
+
+            for i in range(len(row) - 1):
+                for j in range(i + 1, len(row)):
+                    num1 = row[i]
+                    num2 = row[j]
+
+                    if num1 % num2 == 0:
+                        checksum = checksum + (num1 / num2)
+
+                    elif num2 % num1 == 0:
+                        checksum = checksum + (num2 / num1)
+
+    return checksum
